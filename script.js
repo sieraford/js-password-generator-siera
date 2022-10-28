027a5644eca8f06b63364f0f9868e72d2afc492d
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-10-28 13:38:41 -0700

    add variables for types of characters and add prompt for number of characters

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 // Assignment code here
+// lowercase, uppercase, numeric, and/or special characters
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+var lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var specialCharacters = ['!', '"', '#','$', '%', '&','(',')',',',"'",')','*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?','@','[',"]",",",'^','_','`','{','|','}','~']
+var generatePassword = function () {
+  //Ask user for desired length of password
+  var numberOfCharacters = window.prompt("Length of password? Must be at least 8 characters and no more than 128 characters.");
+
+  if (!userChoice) {
+    return;
+  }
+  
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    alert("Invalid number of characters. Please try again.")
+  }
+
+  
+}
+
+
 
 
 // Get references to the #generate element
@@ -15,3 +36,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
