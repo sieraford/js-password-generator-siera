79296f8239ba0159df756d664894277db9621865
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-10-30 20:28:11 -0700

    simplify conditional logic and add validation for lower case letters entries

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var upperCaseLetters = lowerCaseLetters.map(function(x) { return x.toUpperCase()
 var specialCharacters = ['!', '"', '#','$', '%', '&','(',')',',',"'",')','*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?','@','[',"]",",",'^','_','`','{','|','}','~'];
 
 var numberOfCharacters;
+var selectedCriteria = [];
 
 // Helper function to randomize password
 var randomizePass = function (chars) { 
@@ -19,127 +20,156 @@ var randomizePass = function (chars) {
 
 // Function that generates password based off prompted user input
 var generatePassword = function () {
-  //Ask user for desired length of password
+  // Ask user for desired length of password
   numberOfCharacters = window.prompt("Length of password? Must be at least 8 characters and no more than 128 characters.");
 
-  // If user pressed Cancel, immediately end function
+  // Immediately end function if user clicks cancel
   if (!numberOfCharacters) {
     return;
   }
 
-  //Alert user if invalid number of characters were entered
+  // Alert user if invalid number of characters were entered
   if (numberOfCharacters < 8 ||  numberOfCharacters > 128 || isNaN(numberOfCharacters)) {
-    console.log(isNaN(numberOfCharacters));
     alert("Invalid number of characters. Please try again.")
     return;
   } 
 
-  //Ask user if lower case letters should be included
+  // Ask user if lower case letters should be included
   var includeLowerCaseLetters = prompt("Include lower case letters? Y or N")
+  
+  // Immediately end function if users clicks cancel
   if (!includeLowerCaseLetters) {
-    return;
+    return; 
+  }
+    
+  // Ask user to try again if they enter an invalid entry 
+   while(includeLowerCaseLetters != "Y" && includeLowerCaseLetters != "N") {
+    alert("Invalid entry. Please try again.")
+    includeLowerCaseLetters = prompt("Include lower case letters? Y or N")
+  } 
+
+  // Include character type in selected criteria if user enters Y
+  if (includeLowerCaseLetters == "Y") {
+      selectedCriteria = selectedCriteria.concat(lowerCaseLetters);
   }
 
   //Ask user if upper case letter should be included
   var includeUpperCaseLetters = prompt("Include upper case letters? Y or N")
   if (!includeUpperCaseLetters) {
     return;
+  } 
+  
+  if (includeUpperCaseLetters == "Y") {
+    selectedCriteria = selectedCriteria.concat(upperCaseLetters);
   }
 
   // Ask user if numbers should be included
   var includeNumbers = prompt("Include numbers? Y or N")
   if (!includeNumbers) {
     return;
+  } else if (includeNumbers == "Y") {
+    selectedCriteria = selectedCriteria.concat(numbers);
   }
 
   // Ask user if special characters should be included
   var includeSpecialCharacters = prompt("Include special characters? Y or N")
   if (!includeSpecialCharacters) {
     return;
+  } else if (includeSpecialCharacters == "Y") {
+    selectedCriteria = selectedCriteria.concat(specialCharacters);
   }
+
+  if(selectedCriteria === []) {
+    return alert("Invalid entry. Please try again.")
+  }
+
+  return randomizePass(selectedCriteria);
 
   // Create password
 
-  // All types of characters included
-  if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "Y" && includeSpecialCharacters == "Y") {
-    var characters = numbers.concat(lowerCaseLetters).concat(upperCaseLetters).concat(specialCharacters);
-    return randomizePass(characters);
+
+
+
+  // // All types of characters included
+  // if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "Y" && includeSpecialCharacters == "Y") {
+  //   var characters = numbers.concat(lowerCaseLetters).concat(upperCaseLetters).concat(specialCharacters);
+  //   return randomizePass(characters);
     
-  // No special characters
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
-      var characters = numbers.concat(lowerCaseLetters).concat(upperCaseLetters);
+  // // No special characters
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
+  //     var characters = numbers.concat(lowerCaseLetters).concat(upperCaseLetters);
 
-      return randomizePass(characters);
+  //     return randomizePass(characters);
 
-  // No numbers
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
-      var characters = lowerCaseLetters.concat(upperCaseLetters).concat(specialCharacters);
-      return randomizePass(characters);
+  // // No numbers
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
+  //     var characters = lowerCaseLetters.concat(upperCaseLetters).concat(specialCharacters);
+  //     return randomizePass(characters);
 
-  // No lowercase letters
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
-      var characters = numbers.concat(upperCaseLetters).concat(specialCharacters);
-      return randomizePass(characters);
+  // // No lowercase letters
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
+  //     var characters = numbers.concat(upperCaseLetters).concat(specialCharacters);
+  //     return randomizePass(characters);
 
-  // No uppercase letters  
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "Y") {
-      var characters = numbers.concat(lowerCaseLetters).concat(specialCharacters);
-      return randomizePass(characters);
+  // // No uppercase letters  
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "Y") {
+  //     var characters = numbers.concat(lowerCaseLetters).concat(specialCharacters);
+  //     return randomizePass(characters);
   
-  // No lowercase or uppercase letters
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "Y") {
-      var characters = numbers.concat(specialCharacters);
-      return randomizePass(characters);
+  // // No lowercase or uppercase letters
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "Y") {
+  //     var characters = numbers.concat(specialCharacters);
+  //     return randomizePass(characters);
   
-  // No lowercase letters or numbers
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
-      var characters = upperCaseLetters.concat(specialCharacters);
-      return randomizePass(characters);
+  // // No lowercase letters or numbers
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
+  //     var characters = upperCaseLetters.concat(specialCharacters);
+  //     return randomizePass(characters);
   
-  // No lowercase letters or special characters
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
-      var characters = upperCaseLetters.concat(numbers);
-      return randomizePass(characters);
+  // // No lowercase letters or special characters
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
+  //     var characters = upperCaseLetters.concat(numbers);
+  //     return randomizePass(characters);
   
-  // No uppercase letters or numbers
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
-      var characters = lowerCaseLetters.concat(specialCharacters);
-      return randomizePass(characters);
+  // // No uppercase letters or numbers
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
+  //     var characters = lowerCaseLetters.concat(specialCharacters);
+  //     return randomizePass(characters);
 
-  // No uppercase letters or special characters
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
-      var characters = lowerCaseLetters.concat(numbers);
-      return randomizePass(characters);
+  // // No uppercase letters or special characters
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
+  //     var characters = lowerCaseLetters.concat(numbers);
+  //     return randomizePass(characters);
 
-  // No special characters or numbers
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "N") {
-      var characters = upperCaseLetters.concat(lowerCaseLetters);
-      return randomizePass(characters);
+  // // No special characters or numbers
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "N") {
+  //     var characters = upperCaseLetters.concat(lowerCaseLetters);
+  //     return randomizePass(characters);
   
-  // Only lowercase letters
-  } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "N" && includeSpecialCharacters == "N") {
-      var characters = lowerCaseLetters;
-      return randomizePass(characters);
+  // // Only lowercase letters
+  // } else if (includeLowerCaseLetters == "Y" && includeUpperCaseLetters == "N" && includeNumbers == "N" && includeSpecialCharacters == "N") {
+  //     var characters = lowerCaseLetters;
+  //     return randomizePass(characters);
 
-  // Only uppercase letters
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "N") {
-      var characters = upperCaseLetters;
-      return randomizePass(characters);
+  // // Only uppercase letters
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "Y" && includeNumbers == "N" && includeSpecialCharacters == "N") {
+  //     var characters = upperCaseLetters;
+  //     return randomizePass(characters);
   
-  // Only numbers
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
-      var characters = numbers;
-      return randomizePass(characters);
+  // // Only numbers
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "N" && includeNumbers == "Y" && includeSpecialCharacters == "N") {
+  //     var characters = numbers;
+  //     return randomizePass(characters);
 
-  // Only special characters
-  } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "N" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
-      var characters = specialCharacters;
-      return randomizePass(characters);
+  // // Only special characters
+  // } else if (includeLowerCaseLetters == "N" && includeUpperCaseLetters == "N" && includeNumbers == "N" && includeSpecialCharacters == "Y") {
+  //     var characters = specialCharacters;
+  //     return randomizePass(characters);
   
-  // No character type selected
-  } else {     
-      alert("Invalid entry. Please try again.")
-  }
+  // // No character type selected
+  // } else {     
+  //     alert("Invalid entry. Please try again.")
+  // }
 }
 
 // Get references to the #generate element
